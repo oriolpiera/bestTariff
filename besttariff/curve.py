5962e5391e87f023e70ca1ad540f5f51907e3cab
Oriol Piera <oriol.piera@somenergia.coop>
2020-03-14 10:44:13 +0100

    Add TariffPeriod model

--- besttariff/curve.py
+++ besttariff/curve.py
@@ -12,3 +12,18 @@ class Curve:
     def dayOfWeek(self):
         return self.date.weekday()
 
+class TariffPeriod:
+    """Model TariffPeriod represents a tariff period on specific time and price
+    @param  start_hour      int from 0 to 23
+    @param  end_hour        int from 1 to 24
+    @param  day_of_week     int from 0 to 6
+    @param  kwh_price       float(1.0)
+    """
+    def __init__(self, start_hour, end_hour, day_of_week, kwh_price):
+        self.start_hour = start_hour
+        self.end_hour = end_hour
+        self.day_of_week = day_of_week
+        self.kwh_price = kwh_price
+    
+    def getPrice(self):
+        return self.kwh_price
--- tests/curve_tests.py
+++ tests/curve_tests.py
@@ -2,13 +2,22 @@ from besttariff.curve import *
 from datetime import datetime, timedelta
 import unittest
 
-class Test(unittest.TestCase):
+class CurveTest(unittest.TestCase):
     def test__dayOfWeek__Ok(self):
         c = Curve(datetime(2020, 1, 1, 0, 0), 0.0)
 
         d = c.dayOfWeek()
 
         self.assertEqual(d, 2)
+
+
+class TariffPeriodTest(unittest.TestCase):
+    def test__getPrice__Ok(self):
+        tp = TariffPeriod(0,24,0, 0.131)
+
+        price = tp.getPrice()
+
+        self.assertEqual(price, 0.131)
 
 if __name__ == '__main__':
     unittest.main()
